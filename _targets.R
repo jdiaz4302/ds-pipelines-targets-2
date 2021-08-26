a84464dc26d4c3e715f4685ab09ca9c3a3ab0c26
Jeremy Diaz <jeremy.diaz@colorado.edu>
2021-08-26 16:17:37 -0400

    Update _target.R instructions for function reworks

--- _targets.R
+++ _targets.R
@@ -6,37 +6,79 @@ source("3_visualize/src/plot_timeseries.R")
 options(tidyverse.quiet = TRUE)
 tar_option_set(packages = c("tidyverse", "dataRetrieval")) # Loading tidyverse because we need dplyr, ggplot2, readr, stringr, and purrr
 
+parameter_cd = "00010"
+start_date = "2014-05-01"
+end_date = "2015-05-01"
+
 p1_targets_list <- list(
   tar_target(
-    site_data,
-    download_nwis_data(),
+    site_data_01427207,
+    download_nwis_site_data("01427207",
+                            parameterCd = parameter_cd,
+                            startDate = start_date,
+                            endDate = end_date),
+    format = "file"
+  ),
+  tar_target(
+    site_data_01432160,
+    download_nwis_site_data("01432160",
+                            parameterCd = parameter_cd,
+                            startDate = start_date,
+                            endDate = end_date),
+    format = "file"
+  ),
+  tar_target(
+    site_data_01435000,
+    download_nwis_site_data("01435000",
+                            parameterCd = parameter_cd,
+                            startDate = start_date,
+                            endDate = end_date),
+    format = "file"
+  ),
+  tar_target(
+    site_data_01436690,
+    download_nwis_site_data("01436690",
+                            parameterCd = parameter_cd,
+                            startDate = start_date,
+                            endDate = end_date),
+    format = "file"
+  ),
+  tar_target(
+    site_data_01466500,
+    download_nwis_site_data("01466500",
+                            parameterCd = parameter_cd,
+                            startDate = start_date,
+                            endDate = end_date),
+    format = "file"
+  ),
+  tar_target(
+    merged_site_data,
+    merge_nwis_site_data(c(site_data_01427207,
+                           site_data_01432160,
+                           site_data_01435000,
+                           site_data_01436690,
+                           site_data_01466500)),
   ),
   tar_target(
     site_info_csv,
-    nwis_site_info(fileout = "1_fetch/out/site_info.csv", site_data),
+    nwis_site_info(fileout = "1_fetch/out/site_info.csv", merged_site_data),
     format = "file"
   )
 )
 
 p2_targets_list <- list(
   tar_target(
-    site_data_clean, 
-    process_data(site_data)
-  ),
-  tar_target(
-    site_data_annotated,
-    annotate_data(site_data_clean, site_filename = site_info_csv)
-  ),
-  tar_target(
-    site_data_styled,
-    style_data(site_data_annotated)
+    processed_data,
+    process_data(merged_site_data,
+                 site_info_csv)
   )
 )
 
 p3_targets_list <- list(
   tar_target(
     figure_1_png,
-    plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png", site_data_styled),
+    plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png",
+                         processed_data),
     format = "file"
   )
 )
