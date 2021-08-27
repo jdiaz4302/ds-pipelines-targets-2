53d9e9a61d9d88696ef1b1c741da2dcfeb9a6036
Jeremy Diaz <jeremy.diaz@colorado.edu>
2021-08-27 11:34:47 -0400

    Add rds file write to 2_process

--- 2_process/src/process_and_style.R
+++ 2_process/src/process_and_style.R
@@ -1,9 +1,10 @@
-process_data <- function(nwis_data, site_filename){
+process_data <- function(nwis_data, site_filename, out_fpath){
   site_info <- read_csv(site_filename)
   nwis_data_clean <- rename(nwis_data, water_temperature = X_00010_00000) %>% 
     select(-agency_cd, -X_00010_00000_cd, -tz_cd) %>%
     left_join(site_info, by = 'site_no') %>%
     select(station_name = station_nm, site_no, dateTime, water_temperature, latitude = dec_lat_va, longitude = dec_long_va) %>%
     mutate(station_name = as.factor(station_name))
-  return(nwis_data_clean)
+  saveRDS(nwis_data_clean, file = out_fpath)
+  return(out_fpath)
 }
