b9cd2ec7e46d67629f1f0092f530c700c844b643
Jeremy Diaz <jeremy.diaz@colorado.edu>
2021-08-27 11:35:50 -0400

    Update _targets to match file/object substitutions

--- _targets.R
+++ _targets.R
@@ -12,52 +12,47 @@ end_date = "2015-05-01"
 
 p1_targets_list <- list(
   tar_target(
-    site_data_01427207_csv,
+    site_data_01427207,
     download_nwis_site_data("1_fetch/out/nwis_01427207_data.csv",
                             parameterCd = parameter_cd,
                             startDate = start_date,
-                            endDate = end_date),
-    format = "file"
+                            endDate = end_date)
   ),
   tar_target(
-    site_data_01432160_csv,
+    site_data_01432160,
     download_nwis_site_data("1_fetch/out/nwis_01432160_data.csv",
                             parameterCd = parameter_cd,
                             startDate = start_date,
-                            endDate = end_date),
-    format = "file"
+                            endDate = end_date)
   ),
   tar_target(
-    site_data_01435000_csv,
+    site_data_01435000,
     download_nwis_site_data("1_fetch/out/nwis_01435000_data.csv",
                             parameterCd = parameter_cd,
                             startDate = start_date,
-                            endDate = end_date),
-    format = "file"
+                            endDate = end_date)
   ),
   tar_target(
-    site_data_01436690_csv,
+    site_data_01436690,
     download_nwis_site_data("1_fetch/out/nwis_01436690_data.csv",
                             parameterCd = parameter_cd,
                             startDate = start_date,
-                            endDate = end_date),
-    format = "file"
+                            endDate = end_date)
   ),
   tar_target(
-    site_data_01466500_csv,
+    site_data_01466500,
     download_nwis_site_data("1_fetch/out/nwis_01466500_data.csv",
                             parameterCd = parameter_cd,
                             startDate = start_date,
-                            endDate = end_date),
-    format = "file"
+                            endDate = end_date)
   ),
   tar_target(
     merged_site_data,
-    merge_nwis_site_data(c(site_data_01427207_csv,
-                           site_data_01432160_csv,
-                           site_data_01435000_csv,
-                           site_data_01436690_csv,
-                           site_data_01466500_csv)),
+    merge_nwis_site_data(list(site_data_01427207,
+                              site_data_01432160,
+                              site_data_01435000,
+                              site_data_01436690,
+                              site_data_01466500)),
   ),
   tar_target(
     site_info_csv,
@@ -68,17 +63,19 @@ p1_targets_list <- list(
 
 p2_targets_list <- list(
   tar_target(
-    processed_data,
+    processed_data_rds,
     process_data(merged_site_data,
-                 site_info_csv)
+                 site_info_csv,
+                 out_fpath = '2_process/out/processed_data.rds'),
+    format = 'file'
   )
 )
 
 p3_targets_list <- list(
   tar_target(
     figure_1_png,
-    plot_nwis_timeseries(fileout = "3_visualize/out/figure_1.png",
-                         processed_data),
+    plot_nwis_timeseries(out_fpath = "3_visualize/out/figure_1.png",
+                         in_fpath = processed_data_rds),
     format = "file"
   )
 )
