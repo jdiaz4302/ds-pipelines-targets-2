6c39e0475261e8287e90ed6db0bec6e900ad0ec0
Jeremy Diaz <jeremy.diaz@colorado.edu>
2021-08-27 11:35:03 -0400

    Add rds file read to 3_visualize

--- 3_visualize/src/plot_timeseries.R
+++ 3_visualize/src/plot_timeseries.R
@@ -1,8 +1,9 @@
-plot_nwis_timeseries <- function(fileout, site_data_styled, width = 12, height = 7, units = 'in'){
+plot_nwis_timeseries <- function(out_fpath, in_fpath, width = 12, height = 7, units = 'in'){
   
+  site_data_styled <- readRDS(in_fpath)
   ggplot(data = site_data_styled, aes(x = dateTime, y = water_temperature, color = station_name)) +
     geom_line() + theme_bw()
-  ggsave(fileout, width = width, height = height, units = units)
+  ggsave(out_fpath, width = width, height = height, units = units)
   
-  return(fileout)
+  return(out_fpath)
 }
